57f0a4d0bff1c6531d88a20d0ea2955a7b017a45
pastall <pastall@expedia.com>
2015-04-20 09:55:54 +0100

    Updates

--- assets/js/jquery.superslides.js
+++ assets/js/jquery.superslides.js
@@ -7,8 +7,8 @@ var Superslides, plugin = 'superslides';
 
 Superslides = function(el, options) {
   this.options = $.extend({
-    play: false,
-    animation_speed: 600,
+    play: 6000,
+    animation_speed: 800,
     animation_easing: 'swing',
     animation: 'slide',
     inherit_width_from: window,
